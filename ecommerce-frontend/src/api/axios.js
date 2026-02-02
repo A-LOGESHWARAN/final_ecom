564e30b057490cd1ddd1f884c8d0bc312e7cd222
A-LOGESHWARAN <111975057+A-LOGESHWARAN@users.noreply.github.com>
2026-02-02 19:04:33 +0530

    axios

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://final-ecom-au0f.onrender.com/api"
 });
 
 api.interceptors.request.use(config => {
